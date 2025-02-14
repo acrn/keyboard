b0469b0e41c2e455f9d71bfb780f711169e28195
Anders Oskarsen <andersoscarsen@gmail.com>
2025-02-14 10:57:53 +0100

    migth as well push what I've actually been using the last decade

--- modify_xkb_layout.py
+++ modify_xkb_layout.py
@@ -1,105 +1,42 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import re
+import sys
 
-def filter_lines(layout_name, lines, appends=(), **edits):
+MODLINE = "// modified by me"
+DVORAK_RE = re.compile(r'xkb_symbols\s+"dvorak"\s+{')
 
-    '''filters the lines of a symbols file and returns a generator where some
-    of the keybindings have been changed or appended.
 
-    Arguments:
-    layout_name -- The name of the layout in the input to which the filter
-                   should be applied. It is used to match lines like
-                   'xkb_symbols "dvorak" {' with a regex. If no layout matches
-                   in the input no filtering is applied.
-    lines -- An iterable of the input to filter.
-    appends -- Stuff to insert at the end of the layout definition. This
-               argument exists only to enable 'include "level3(ralt_switch)"
+filename = sys.argv[1]
+modify = True
+with open(filename) as f:
+    line = next(f)
+    print(MODLINE)
+    if line.strip() == MODLINE:
+        for line in f:
+            print(line, end='')
+    else:
+        print(line, end='')
+    for line in f:
+        if DVORAK_RE.match(line.strip()):
+            print(line, end='')
+            print("""\
+// BEGIN MODIFIED
+    name[Group1]= "English (Dvorak, modified)";
+    include "us(dvorak-std)"
 
-    Keyword arguments:
-    edits -- The name of a keycode to create or replace in the output and an
-             iterable of the keybindings to bind it to.
-             Example AC01=('a', 'A', 'aring', 'Aring')
-    '''
+    key <AC01> { [ a, A, aring,       Aring      ] };
+    key <AC02> { [ o, O, odiaeresis,  Odiaeresis ] };
+    key <AC03> { [ e, E, adiaeresis,  Adiaeresis ] };
+    key <AC09> { [ n, N, ntilde,      Ntilde     ] };
+    key <AD10> { [ l, L, dead_stroke, dead_acute ] };
+    key <LSGT> { [ dead_diaeresis, dead_abovering, dead_tilde, dead_stroke ] };
+    key <CAPS> { [ Escape ] };
+    include "level3(ralt_switch)"
+};
 
-    def to_xkb_line(keycode, keybindings, indentation='  '):
-        '''creates a string of the type:
-            "  key <AC01> { [ a, A, aring, Aring ] };\n"
-
-        Arguments:
-        keycode -- Something like "AC01"
-        keybindings -- An iterable like ('a', 'A', 'aring', 'Aring')
-
-        Keyword arguments:
-        indentation -- inserted at start of line (default: '  ')
-
-        '''
-
-        return '{}key <{}> {{ [ {} ] }};\n'.format(
-                indentation,
-                keycode,
-                ', '.join(keybindings))
-
-    #matches strings like 'xkb_symbols "dvorak" {'
-    rx_start_of_layout = re.compile(r'\s*xkb_symbols\s*"{}"'.format(
-        layout_name))
-    line = next(lines)
-    # Yield everything preceding the target layout unaltered
-    while not rx_start_of_layout.search(line):
-        yield line
-        line = next(lines)
-
-    indentation = '' 
-    #matches string like "};"
-    rx_end_of_layout = re.compile(r'^[^{]*}\s*;\s')
-    # matches strings like "  key <AC01> { [ a, A ] };"
-    rx_key_definition = re.compile(r'(\s*)(key\s*<\s*)([^\s>]*)')
-    # Filter each line within the target layout
-    while not rx_end_of_layout.search(line):
-        keycode = None
-        rx_search = rx_key_definition.search(line)
-        if rx_search:
-            indentation, _, keycode = rx_search.groups()
-        yield to_xkb_line(keycode, edits.pop(keycode), indentation) \
-                if keycode in edits \
-                else line
-        line = next(lines)
-
-    # When at the end of the layout definition yield all the keyboard
-    # definitions that have yet to be inserted. Sort the keys so the output
-    # stays consistent.
-    for keycode in sorted(edits.keys()):
-        yield to_xkb_line(keycode, edits.pop(keycode), indentation)
-
-    # Tack on everything in appends, this is an ugly hack
-    for append in appends:
-        yield indentation + append.strip() + '\n'
-
-    # Yield everything following the target layout unaltered
-    yield line
-    while True:
-        yield next(lines)
-
-if __name__ == '__main__':
-
-    layout_name = 'dvorak'
-    edits = {
-        'AC01': ('a', 'A', 'aring', 'Aring'),
-        'AC02': ('o', 'O', 'odiaeresis', 'Odiaeresis'),
-        'AC03': ('e', 'E', 'adiaeresis', 'Adiaeresis'),
-        'AC09': ('n', 'N', 'ntilde', 'Ntilde'),
-        'AD10': ('l', 'L', 'dead_stroke', 'dead_acute'),
-        'LSGT': ('dead_diaeresis', 'dead_abovering', 'dead_tilde', 'dead_stroke'),
-        'CAPS': ('Escape',),
-    }
-    appends = (
-        'include "level3(ralt_switch)"',
-    )
-
-    import sys
-    assert len(sys.argv) < 3
-    filename = sys.argv[len(sys.argv) - 1]
-    with open(filename) as _file:
-        lines = _file if len(sys.argv) > 1 else sys.stdin
-        for line in filter_lines(layout_name, lines, appends, **edits):
+partial alphanumeric_keys
+xkb_symbols "dvorak-std" {
+// END MODIFIED""")
+        else:
             print(line, end='')
